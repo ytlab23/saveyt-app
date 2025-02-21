868fd5eb63107aaee473d46bde85015759c1c705
kvnq-poza <kvnqpoza@gmail.com>
2025-02-21 23:15:32 +0100

    Added Video download feature and updated to handle video and mp3 states separately.

--- src/context/VideoContext.jsx
+++ src/context/VideoContext.jsx
@@ -5,17 +5,42 @@ import { createContext, useState, useContext } from "react";
 const VideoContext = createContext();
 
 export const VideoProvider = ({ children }) => {
+  const [mp3Info, setMp3Info] = useState(null);
   const [videoInfo, setVideoInfo] = useState(null);
   const [currentVideoUrl, setCurrentVideoUrl] = useState(null);
+  const [currentMode, setCurrentMode] = useState("mp3");
+
+  const setMp3Data = (info, url) => {
+    setMp3Info(info);
+    setCurrentVideoUrl(url);
+  };
 
   const setVideoData = (info, url) => {
     setVideoInfo(info);
     setCurrentVideoUrl(url);
   };
 
+  const clearVideoData = () => {
+    setVideoInfo(null);
+  };
+
+  const clearMp3Data = () => {
+    setMp3Info(null);
+  };
+
   return (
     <VideoContext.Provider
-      value={{ videoInfo, setVideoInfo: setVideoData, currentVideoUrl }}
+      value={{
+        mp3Info,
+        videoInfo,
+        currentVideoUrl,
+        currentMode,
+        setCurrentMode,
+        setMp3Data,
+        setVideoData,
+        clearVideoData,
+        clearMp3Data,
+      }}
     >
       {children}
     </VideoContext.Provider>

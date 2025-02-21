79921f1ffabce04c83536e5cdd3c427eab6d8ae6
kvnq-poza <kvnqpoza@gmail.com>
2025-02-21 05:36:24 +0100

    Added new package

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ import forms from "@tailwindcss/forms";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%, 100%": { transform: "translate(0, 0) scale(1)" },
+          "50%": { transform: "translate(1%, 1%) scale(1.02)" },
+        },
+      },
+      animation: {
+        gradient: "gradient 10s ease infinite",
+        "gradient-slow": "gradient 15s ease-in-out infinite",
+      },
+    },
   },
   plugins: [forms],
 };

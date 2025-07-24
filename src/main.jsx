3d2d2944466e447dbb6204bc03a535c4c282af24
kvnq-poza <kvnqpoza@gmail.com>
2025-07-25 00:54:08 +0100

    Add ad service worker

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,22 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
+// ✅ Register the Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        // console.log(
+        //   "Service Worker registered with scope:",
+        //   registration.scope
+        // );
+      })
+      .catch((error) => {
+        // console.error("Service Worker registration failed:", error);
+      });
+  });
+}

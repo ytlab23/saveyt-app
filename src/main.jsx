76441ce0afef61407804bff32b5475a43d908f2a
kvnq-poza <kvnqpoza@gmail.com>
2025-07-31 18:14:36 +0100

    Remove old service worker and update page head

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>,
 );
-
-// ✅ Register the Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        // console.log(
-        //   "Service Worker registered with scope:",
-        //   registration.scope
-        // );
-      })
-      .catch((error) => {
-        // console.error("Service Worker registration failed:", error);
-      });
-  });
-}
